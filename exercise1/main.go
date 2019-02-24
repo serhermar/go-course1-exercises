b2f96ded123e1dcc454083d529dca285f7793ce7
serhermar <serhermar@hotmail.com>
2019-02-24 14:21:28 +0100

    Exercise 1

---
+++ exercise1/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+/**
+*	Exercise 1
+**/
+
+func main() {
+	// Section 1
+	x := 42           // a
+	y := "James Bond" // b
+	z := true         // c
+
+	// Section 2
+	fmt.Println(x, y, z) // a
+
+	fmt.Println(x) //b
+	fmt.Println(y) //b
+	fmt.Println(z) //b
+}
